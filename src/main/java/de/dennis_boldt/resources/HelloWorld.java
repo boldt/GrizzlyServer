420e8d31ec73384e07ca0725836c30118f6cbe0d
Dennis Boldt <boldt@itm.uni-luebeck.de>
2013-09-25 16:45:53 +0200

    Use MediaType instead of plain text.

--- src/main/java/de/dennis_boldt/resources/HelloWorld.java
+++ src/main/java/de/dennis_boldt/resources/HelloWorld.java
@@ -3,19 +3,20 @@ package de.dennis_boldt.resources;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/helloworld")
 public class HelloWorld {
 
 	@GET
-	@Produces("text/plain")
+	@Produces(MediaType.TEXT_PLAIN)
 	public Response getTextPlain() {
 		return Response.ok().entity("Hello World").build();
 	}
 
 	@GET
-	@Produces("text/html")
+	@Produces(MediaType.TEXT_HTML)
 	public Response getTextHtml() {
 		return Response.ok().entity("<strong>Hello World</strong>").build();
 	}
