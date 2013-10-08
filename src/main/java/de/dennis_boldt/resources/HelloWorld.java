c34d8dd21ed80d59625ee28317a848b8f86529fa
Dennis Boldt <boldt@itm.uni-luebeck.de>
2013-10-08 17:04:48 +0300

    JSON lists works

--- src/main/java/de/dennis_boldt/resources/HelloWorld.java
+++ src/main/java/de/dennis_boldt/resources/HelloWorld.java
@@ -1,10 +1,15 @@
 package de.dennis_boldt.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.codehaus.jettison.json.JSONArray;
 
 @Path("/helloworld")
 public class HelloWorld {
@@ -12,15 +17,24 @@ public class HelloWorld {
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public Response getTextPlain() {
-		return Response.ok().entity("Hello World").build();
+		return Response.ok().entity("Hello World!").build();
 	}
 
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public Response getTextHtml() {
-		return Response.ok().entity("<strong>Hello World</strong>").build();
+		return Response.ok().entity("<strong>Hello World!</strong>").build();
 	}
 
-
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getTextList()
+	{
+		List<String> list = new ArrayList<String>();
+		list.add("hello");
+		list.add("world");
+		list.add("!");
+		return Response.ok(new JSONArray(list)).build();
+	}
 
 }
